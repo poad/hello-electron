7130ae477931159a34b6646fe82aceb68e852569
Kenji Saito <1867845+poad@users.noreply.github.com>
2021-12-07 21:48:01 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "rules": {
+        'import/no-extraneous-dependencies': ['off'],
+    }
+};
